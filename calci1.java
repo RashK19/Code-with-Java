15336177c57320801f400493835f79f378fbf3f3
Rashmi <118280805+RashK19@users.noreply.github.com>
2024-04-08 21:05:50 +0530

    Simple arithmetic operation
    Arithmetic operations like add, sub, mul and div on static data using Java

---
+++ calci1.java
@@ -0,0 +1,16 @@
+class calci1 
+{
+    public static void main(String [] args)
+    {
+        int a=5,b=4,sum,sub,mul,div;
+        sum=a+b;
+        sub=a-b;
+        mul=a*b;
+        div=a/b;
+        System.out.println("a=5 and b=4");
+        System.out.println("Sum="+sum);
+        System.out.println("Sub="+sub);
+        System.out.println("Mul="+mul);
+        System.out.println("Div="+div);
+    }
+}
